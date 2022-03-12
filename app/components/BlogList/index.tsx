7b6b35331e41d9ab854fabe0c550f12db1de4127
quochuydev <quochuy.dev@gmail.com>
2022-03-13 01:53:55 +0700

    update

--- app/components/BlogList/index.tsx
+++ app/components/BlogList/index.tsx
@@ -4,7 +4,7 @@ import { Link } from "remix";
 
 export default function BlogList({ blogs }: any) {
   return (
-    <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+    <div className="relative bg-gray-50 pt-8 pb-20 px-4 sm:px-6 lg:pt-8 lg:pb-28 lg:px-8">
       <div className="absolute inset-0">
         <div className="bg-white h-1/3 sm:h-2/3" />
       </div>
