edb63678e647f07e2f4f895af65653da958a8b4e
quochuydev <quochuy.dev@gmail.com>
2022-03-13 10:41:04 +0700

    update

---
+++ app/routes/files.tsx
@@ -0,0 +1,63 @@
+import { useMemo } from "react";
+import type { ActionFunction, UploadHandler } from "remix";
+import { json, unstable_parseMultipartFormData } from "remix";
+import { supabase } from "~/utils/supabase.server";
+
+export const action: ActionFunction = async ({ request, params }: any) => {
+  const uploadHandler: UploadHandler = async ({
+    name,
+    stream,
+    filename,
+  }: any) => {
+    console.log("in uploadHandler", name);
+
+    if (name !== "file-upload") {
+      stream.resume();
+      return;
+    } else {
+      console.log(name, filename);
+    }
+
+    const chunks = [];
+    for await (const chunk of stream) chunks.push(chunk);
+    const buffer = Buffer.concat(chunks);
+
+    const fileName = `${Date.now()}-${filename}`;
+
+    const { error } = await supabase.storage
+      .from("images")
+      .upload(fileName, buffer);
+    if (error) {
+      throw error;
+    }
+
+    const { publicURL } = supabase.storage
+      .from("images")
+      .getPublicUrl(fileName);
+
+    return publicURL as string;
+  };
+
+  const formData = await unstable_parseMultipartFormData(
+    request,
+    uploadHandler
+  );
+
+  console.log("uploaded", formData.get("file-upload"));
+
+  const createData = {
+    url: formData.get("file-upload"),
+  };
+
+  const { data, error } = await supabase
+    .from("files")
+    .insert([createData])
+    .single();
+
+  console.log(data, error);
+  return json(data);
+};
+
+export default function NewFile() {
+  return <></>;
+}
