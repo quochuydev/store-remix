709991962dc4810259f1971a58ae9cebdf21d379
quochuydev <quochuy.dev@gmail.com>
2022-03-19 17:17:25 +0700

    update

--- app/components/Category/index.tsx
+++ app/components/Category/index.tsx
@@ -1,33 +1,32 @@
 import { useCallback, useEffect, useState } from "react";
-import { supabaseClient } from "~/supabase";
-const supabase = supabaseClient;
+import { supabaseClient as supabase } from "~/supabase";
 
 export default function Category({ name = "", value = "", onData }: any) {
   const [categories, setCategories] = useState<any>([]);
-  const [category, setCategory] = useState<any>({ name: "" });
+  const [category, setCategory] = useState<any>({ id: null, name: "" });
 
   const getCategories = useCallback(async () => {
     const { data } = await supabase.from<any>("categories").select("*");
     setCategories(data);
   }, []);
 
-  const createCategory = useCallback(async () => {
-    if (!category.name) {
+  const createCategory = useCallback(async (data) => {
+    if (!data.name) {
       return;
     }
 
-    const { data, error } = await supabase
+    await supabase
       .from("categories")
       .insert([
         {
-          name: category.name,
+          name: data.name,
         },
       ])
       .single();
 
     setCategory({ name: "" });
     getCategories();
-  }, [category]);
+  }, []);
 
   useEffect(() => {
     getCategories();
@@ -37,18 +36,23 @@ export default function Category({ name = "", value = "", onData }: any) {
     <div>
       <input
         type="text"
-        value={category.name}
+        value={category?.name}
         onChange={(e) => setCategory({ name: e.target.value })}
         className={`max-w-lg block w-full shadow-md focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-500 rounded-md`}
       />
-      <button type="button" onClick={() => createCategory()}>
+      <button type="button" onClick={() => createCategory(category)}>
         +
       </button>
       <select
         name={name}
         id={name}
         value={value}
-        onChange={(e) => onData && onData(e.target.value)}
+        onChange={(event) => {
+          setCategory(
+            categories.find((e: any) => e?.id === Number(event.target.value))
+          );
+          onData && onData(event.target.value);
+        }}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       >
         <option key={"none"} value={""}>
